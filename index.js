654a3d61ba30d05aaa3b2d86d7eaf55db57f7fd0
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-27 04:46:21 +0600

    total sell data load

--- index.js
+++ index.js
@@ -29,35 +29,60 @@ async function run() {
         const paymentCollection = client.db("TreeTreasuresDB").collection("payments");
         const checkOutCollection = client.db("TreeTreasuresDB").collection("checkOut");
         const salesCollection = client.db("TreeTreasuresDB").collection("sales");
+        // db.salesCollection.find().sort({ soldTime: -1 })
+
+
 
 
 
         // check manager
-         
-    app.get("/users/:email", async (req, res) => {
-        const email = req.params.email
-        // if(email !== res?.decoded?.email){
-        //   return res.status(403).send({message: "Forbidden access"})
-        // }
-        const query = { email: email }
-        const user = await usersCollection.findOne(query)
-        let manager = false
-        if (user) {
-          manager = user?.role === "manager"
-        }
-        res.send({ manager })
-      })
-    app.get("/manager/:email", async (req, res) => {
-        const email = req.params.email
-        // if(email !== res?.decoded?.email){
-        //   return res.status(403).send({message: "Forbidden access"})
-        // }
-        const query = { email: email }
-        const manager = await usersCollection.findOne(query)
-       
-        res.send(manager)
-      })
+        //  isUser
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email
+            // if(email !== res?.decoded?.email){
+            //   return res.status(403).send({message: "Forbidden access"})
+            // }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let manager = false
+            if (user) {
+                manager = user?.role === "manager"
+            }
+            res.send({ manager })
+        })
+
+        //   isAdmin
+        app.get("/users/admin/:email", async (req, res) => {
+            const email = req.params.email
+            // if(email !== res?.decoded?.email){
+            //   return res.status(403).send({message: "Forbidden access"})
+            // }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === "Admin"
+            }
+            res.send({ admin })
+        })
+        app.get("/manager/:email", async (req, res) => {
+            const email = req.params.email
+            // if(email !== res?.decoded?.email){
+            //   return res.status(403).send({message: "Forbidden access"})
+            // }
+            const query = { email: email }
+            const manager = await usersCollection.findOne(query)
+
+            res.send(manager)
+        })
+
+
+
+        // ---------------------------------------
         // user related api
+        // ---------------------------------------
+
+
         app.post("/users", async (req, res) => {
             const users = req.body
             const query = { email: users.email }
@@ -105,11 +130,14 @@ async function run() {
         })
 
 
-
+        // ---------------------------------------
         //   shop related api
+        // ---------------------------------------
+
+
         app.post("/shopData", async (req, res) => {
             const shopData = req.body
-            const query = {shopOwnerEmail: shopData.shopOwnerEmail}
+            const query = { shopOwnerEmail: shopData.shopOwnerEmail }
             const existingUser = await shopCollection.findOne(query)
             if (existingUser) {
                 return res.send({ message: "Shop already exist", insertedId: null })
@@ -118,23 +146,29 @@ async function run() {
             res.send(result)
         })
 
+
+
+        // ---------------------------------------
         // Product related api
+        // ---------------------------------------
+
+
         app.post("/products", async (req, res) => {
             const products = req.body
             const result = await productCollection.insertOne(products)
             res.send(result)
         })
 
-        app.get("/products/:email", async(req, res)=>{
+        app.get("/products/:email", async (req, res) => {
             const email = req.params.email
-            const query = {email: email}
+            const query = { email: email }
             const result = await productCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.get("/singleProduct/:id", async(req, res)=>{
+        app.get("/singleProduct/:id", async (req, res) => {
             const id = req.params.id
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await productCollection.findOne(query)
             res.send(result)
         })
@@ -145,30 +179,30 @@ async function run() {
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    productName:data?.productName,
-                    productImage:data?.productImage,
-                    productQuantity:data?.productQuantity,
-                    productLocation:data?.productLocation,
-                    profitMargin:data?.profitMargin,
-                    makingCost:data?.makingCost,
-                    productPrice:data?.productPrice,
-                    productDiscount:data?.productDiscount,
-                    productDescription:data?.productDescription,
+                    productName: data?.productName,
+                    productImage: data?.productImage,
+                    productQuantity: data?.productQuantity,
+                    productLocation: data?.productLocation,
+                    profitMargin: data?.profitMargin,
+                    makingCost: data?.makingCost,
+                    productPrice: data?.productPrice,
+                    productDiscount: data?.productDiscount,
+                    productDescription: data?.productDescription,
                 }
             }
 
-            const result = await usersCollection.updateOne(query, updateDoc, options)
+            const result = await productCollection.updateOne(query, updateDoc, options)
             res.send(result)
         })
 
-        app.delete('/productDelete/:id',  async (req, res) => {
+        app.delete('/productDelete/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await productCollection.deleteOne(query)
             res.send(result)
-          })
+        })
 
-          app.patch('/product/:id', async(req, res)=>{
+        app.patch('/product/:id', async (req, res) => {
             const id = req.params.id
             const data = req.body
             const query = { _id: new ObjectId(id) }
@@ -182,77 +216,107 @@ async function run() {
 
             const result = await productCollection.updateOne(query, updateDoc, options)
             res.send(result)
-          })
-// check out related api
-app.post("/product-check-out", async(req, res)=>{
-    const product = req.body
-    const result = await checkOutCollection.insertOne(product)
-    res.send(result)
-})
+        })
+        // ---------------------------------------
+        // check out related api
+        // ---------------------------------------
+        app.post("/product-check-out", async (req, res) => {
+            const product = req.body
+            const result = await checkOutCollection.insertOne(product)
+            res.send(result)
+        })
 
-app.get('/product-check-out/:email', async(req, res)=>{
-    const email = req.params.email
-    const query ={email: email}
-    const result = await checkOutCollection.find(query).toArray()
-    res.send(result)
+        app.get('/product-check-out/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await checkOutCollection.find(query).toArray()
+            res.send(result)
 
-})
+        })
 
-
-
-// sell collection
-
-app.post('/sold-product', async(req, res)=>{
-    const product = req.body
-    const result = await salesCollection.insertOne(product)
-    res.send(result)
-
-})
-app.delete('/sold-product-delete/:id', async(req, res)=>{
-    const id = req.params.id
-    const query = { _id: new ObjectId(id) }
-    const result = await checkOutCollection.deleteOne(query)
-    res.send(result)
-
-    
-})
+        app.delete('/sold-product-delete/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await checkOutCollection.deleteOne(query)
+            res.send(result)
 
 
-// ................................
-// 
-// .................................
- // payment intents er jonno post request korte hobe
- app.post("/create-payment-intent", async (req, res) => {
-    const { price } = req.body;
-    const amount = parseInt(price * 100)
+        })
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "usd",
-      payment_method_types: ["card"],
-    });
+        // ---------------------------------------
+        // sell collection 
+        // ---------------------------------------
+        app.post('/sold-product', async (req, res) => {
+            const product = req.body
+            const result = await salesCollection.insertOne(product)
+            res.send(result)
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  })
-  
-      app.put("/payment", async(req, res)=>{
-        const item = req.body
-        const paymentResult = await paymentCollection.insertOne(item)
-  
-        
-        res.send(paymentResult)
-      })
-  
-  
-  
-      app.get('/paymentStatus/:email', async(req, res)=>{
-        const email = req.params.email
-        const query = {email: email}
-        const result = await paymentCollection.findOne(query)
-        res.send(result)
-      })
+        })
+
+        app.get('/sell-summary/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const page = req.query.page
+            console.log(page);
+
+            const productResult = await salesCollection.find(query).toArray()
+            const totalInvest = Math.floor(productResult?.reduce(
+                (total, item) => total + parseFloat(item?.makingCost),
+                0
+            ));
+            const totalIncome = productResult?.reduce(
+                (total, item) => total + parseFloat(item?.productPrice),
+                0
+            );
+            const totalProfit = Math.floor(totalIncome - totalInvest);
+            const totalProduct = productResult.length;
+            console.log(totalProduct);
+            const pageNumber = parseInt(page);
+            console.log(pageNumber);
+            const perPage = 5;
+            const skip = perPage * pageNumber;
+            
+            const  result = await salesCollection.find(query).skip(skip).limit(perPage).toArray()
+
+
+            res.send({ result, totalProduct, totalInvest, totalProfit })
+        })
+
+        // ................................
+        // payment related api
+        // .................................
+        // payment intents er jonno post request korte hobe
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100)
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+        app.put("/payment", async (req, res) => {
+            const item = req.body
+            const paymentResult = await paymentCollection.insertOne(item)
+
+
+            res.send(paymentResult)
+        })
+
+
+
+        app.get('/paymentStatus/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await paymentCollection.findOne(query)
+            res.send(result)
+        })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally { }
